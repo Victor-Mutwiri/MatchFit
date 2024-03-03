7801b46438e06d331229b7858667773cba382cbe
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-03-03 23:53:52 +0300

    3.2

--- MatchFit/src/pages/User/UseJobTypes.jsx
+++ MatchFit/src/pages/User/UseJobTypes.jsx
@@ -7,7 +7,9 @@ export const useJobTypes = () => {
 
     const fetchJobType = async () => {
         try {
-            const {data:{data}} = await axios.get('http://localhost:1337/api/jobtypes')
+            const baseUrl = import.meta.env.DEV ? import.meta.env.VITE_DEV_API_BASE_URL : import.meta.env.VITE_PROD_API_BASE_URL;
+            const {data:{data}} = await axios.get(`${baseUrl}/jobtypes`)
+            /* const {data:{data}} = await axios.get('http://localhost:1337/api/jobtypes') */
             setJobType(data)
             /* setJobType(response.data) */
         } catch (error) {
@@ -17,7 +19,9 @@ export const useJobTypes = () => {
 
     const fetchJobs = async () => {
         try {
-            const {data:{data}} = await axios.get('http://localhost:1337/api/jobs?populate=*')
+            const baseUrl = import.meta.env.DEV ? import.meta.env.VITE_DEV_API_BASE_URL : import.meta.env.VITE_PROD_API_BASE_URL;
+            const {data:{data}} = await axios.get(`${baseUrl}/jobs?populate=*`)
+            /* const {data:{data}} = await axios.get('http://localhost:1337/api/jobs?populate=*') */
             setJobs(data)
         } catch (error) {
             console.log(error)
