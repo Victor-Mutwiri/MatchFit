60ce8d779dd75900f3da0d59ccc7185f40c26437
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-03-07 15:31:04 +0300

    3.6

--- MatchFit/src/components/Navbar.jsx
+++ MatchFit/src/components/Navbar.jsx
@@ -7,11 +7,10 @@ export const Navbar = () => {
   return (
     <header>
         <div className="logo left">
-            <a href=""></a>
             <Link to='/'><img src={logo} alt="logo" width={140}/></Link>
             {/* <img src={logo} alt="logo" width={140}/> */}
         </div>
-        <div className="left">
+        <div className="left left-options">
             {/* <li><Link to='/UserLogin'>Suggested Jobs</Link></li> */} {/* Feature paused for now */}
             <li><Link to='/jobs'>Suggested Jobs</Link></li>
             <li><Link to='/EmployerLogin'>Post a Job</Link></li>
@@ -19,6 +18,11 @@ export const Navbar = () => {
         <div className="center">
             {/* <li><Link to='/UserLogin'><i className='bx bxs-user'/> Job Seekers</Link></li> */} {/* Feature paused for now */}
             <li><Link to='/jobs'><i className='bx bxs-user'/> Job Seekers</Link></li>
+            <li><Link to='/EmployerLogin'><i className='bx bx-buildings'/> Employers</Link></li>
+        </div>
+        <div className="center-mobile">
+            {/* <li><Link to='/UserLogin'><i className='bx bxs-user'/> Job Seekers</Link></li> */} {/* Feature paused for now */}
+            <li><Link to='/jobs'><i className='bx bxs-user'/> Jobs</Link></li>
             <li><Link to='/EmployerLogin'><i className='bx bx-buildings'/> Employers</Link></li>
         </div>
         <i className='bx bx-menu right'/>
